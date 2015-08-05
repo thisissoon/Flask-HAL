c40e48ac53fc6afc81062c9a91341b1314885b42
Chris Reeves <hello@chris.reeves.io>
2015-08-05 14:23:55 +0100

    Build link attributes from valid keyword arguments

--- flask_hal/link.py
+++ flask_hal/link.py
@@ -9,28 +9,57 @@ Implements the ``HAL`` Link specification.
 """
 
 
+VALID_LINK_ATTRS = [
+    'name',
+    'title',
+    'type',
+    'deprecation',
+    'profile',
+    'templated',
+    'hreflang'
+]
+
+
 class Link(object):
-    """
+    """Build ``HAL`` specification ``_links`` object.
+
+    Example:
+        >>> from flask_hal.link import Link
+        >>> l = Link('foo', 'http://foo.com/bar')
+        >>> print l.to_json()
+        ... '{"foo": {"href": "http://foo.com/bar"}}'
+        >>> l.title = 'Foo'
+        >>> print l.to_json()
+        ... '{"foo": {"href": "http://foo.com/bar", "name": "Foo"}}'
+
     """
 
-    def __init__(
-            self,
-            href,
-            name=None,
-            title=None,
-            link_type=None,
-            deprecation=None,
-            profile=None,
-            templated=None,
-            hreflang=None):
-        """
+    def __init__(self, rel, href, **kwargs):
+        """Initialise a new ``Link`` object.
+
+        Args:
+            rel (str): The links ``rel`` or name
+            href (str): The URI to the resource
+
+        Keyword Args:
+            name (str): The links name attribute, optional
+            title (str): The links title attribute, optional
+            type (str): The links type attribute, optional
+            deprecation (str): The deprecation attribute, optional
+            profile (str): The profile  attribute, optional
+            templated (bool): The templated attribute, optional
+            hreflang (str): The hreflang attribute, optional
         """
 
+        self.rel = rel
         self.href = href
-        self.name = name
-        self.title = title
-        self.link_type = link_type,
-        self.deprecation = deprecation
-        self.profile = profile
-        self.templated = templated
-        self.hreflang = hreflang
+
+        for attr in VALID_LINK_ATTRS:
+            if attr in kwargs:
+                setattr(self, attr, kwargs.pop(attr))
+
+    def to_json(self):
+        """Returns the ``JSON`` encoded representation of the ``Link`` object.
+        """
+
+        pass
