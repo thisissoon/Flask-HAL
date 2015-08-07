b1506e0fe02f453c42b81359ca4ee5eaddc9451e
Chris Reeves <hello@chris.reeves.io>
2015-08-07 11:01:20 +0100

    More attributes in setup.py for PyPi

--- setup.py
+++ setup.py
@@ -1,19 +1,57 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+"""
+Flask-HAL
+---------
+
+Provides easy integration of the  `HAL <https://tools.ietf.org/html/draft-kelly-json-hal-07>`_
+specification for your ``REST`` Flask Applications.
+"""
+
+# Third Party Libs
 from setuptools import setup
 
 
+# Generate a Long Decription for the PyPi page which is the README.rst
+# Plus the CHANGELOG.rst
+long_description = open('README.rst').read()
+changelog = open('CHANGELOG.rst').read()
+long_description += '\n' + changelog
+
+
 setup(
-    name='flask_hal',
-    version='0.0.1',
-    description='Flask HAL',
-    author='SOON_',
-    author_email='soon@thisissoon.com',
+    name='Flask-HAL',
     url='https://github.com/thisissoon/Flask-HAL',
+    version='0.0.1',
+    author='SOON_',
+    author_email='dorks@thisissoon.com',
+    description='Provides easy integration of the HAL specification for '
+                'your REST Flask Applications.',
+    long_description=long_description,
     packages=[
         'flask_hal',
-    ],
-    classifiers=[
     ],
     install_requires=[
         'flask',
     ],
+    classifiers=[
+        'Framework :: Flask',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 5 - Production/Stable',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ],
+    license='MIT',
+    keywords=['Flask', 'HAL', 'REST', 'Views']
 )
