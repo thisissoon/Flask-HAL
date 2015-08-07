39a636587813bb486ddca161a92dac30f2487e92
Chris Reeves <hello@chris.reeves.io>
2015-08-07 14:34:36 +0100

    Add Unittests for Self class

--- tests/test_link.py
+++ tests/test_link.py
@@ -11,8 +11,11 @@ Unittests for the :module:`flask_hal.link` module.
 # Standard Libs
 import json
 
+# Third Party Libs
+from flask import Flask
+
 # First Party Libs
-from flask_hal.link import Link
+from flask_hal.link import Link, Self
 
 
 class TestLink(object):
@@ -46,3 +49,42 @@ class TestLink(object):
         })
 
         assert l.to_json() == expected
+
+
+class TestSelf(object):
+
+    def setup(self):
+        self.app = Flask(__name__)
+
+    def test_only_valid_link_attrs_set(self):
+        with self.app.test_request_context():
+            l = Self(foo='foo', name='foo')
+
+            assert not hasattr(l, 'foo')
+            assert l.name == 'foo'
+
+    def test_to_dict(self):
+        with self.app.test_request_context():
+            l = Self(foo='foo', name='foo')
+
+            expected = {
+                'self': {
+                    'href': 'http://localhost/',
+                    'name': 'foo',
+                }
+            }
+
+            assert l.to_dict() == expected
+
+    def test_to_json(self):
+        with self.app.test_request_context():
+            l = Self(foo='foo', name='foo')
+
+            expected = json.dumps({
+                'self': {
+                    'href': 'http://localhost/',
+                    'name': 'foo',
+                }
+            })
+
+            assert l.to_json() == expected
